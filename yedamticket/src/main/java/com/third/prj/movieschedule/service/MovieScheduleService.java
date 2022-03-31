fdd7e5fc032b9328b3da5f17ea542f99660466d6
rbqhr <rbqhr5452@gmail.com>
2022-03-31 19:26:49 +0900

    0331 re

---
+++ yedamticket/src/main/java/com/third/prj/movieschedule/service/MovieScheduleService.java
@@ -0,0 +1,9 @@
+package com.third.prj.movieschedule.service;
+
+import java.util.List;
+
+public interface MovieScheduleService {
+	List<MovieScheduleVO> movieSchdtList(MovieScheduleVO vo);
+		
+	
+}
